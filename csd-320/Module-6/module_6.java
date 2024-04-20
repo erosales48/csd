c4369d71d725fcc8c394b64c1a9838d63fdb9bf5
erosales48 <157036707+erosales48@users.noreply.github.com>
2024-04-19 21:26:57 -0400

    fixed formatting, final working

--- csd-320/Module-6/module_6.java
+++ csd-320/Module-6/module_6.java
@@ -1,48 +1,44 @@
+/* Edgar D Rosales
+ * 19 Apr 2024
+ * CSD-320-J318 Programming with Java (2245-DD)
+ * Module 6.2 Assignment
+ * Write a program that uses nested for loops to output the following with the shown display format:
+ * (display omitted)
+ */
 public class module_6 {
     public static void main(String[] args) {
         // Define the number of rows
-        int rows = 7;
+        int maxRows = 7;
 
-        // Loop through each row
-        for (int space = 0; space < rows; space++) {
+        // Loop through each line until max rows is hit
+        for (int line = 0; line < maxRows; line++) {
 
-            // Print spaces for formatting
-            if (space < 5) {
-                System.out.print("  ");
-            }
-            if (space == 5) {
+            // Print spaces for formatting       
+            for (int column = 0; column < 3 * (maxRows - line - 1); column++) {
                 System.out.print(" ");
             }
-            
-            for (int column = 0; column < 2 * (rows - space - 1); column++) {
-                System.out.print(" ");
-            }
-            
-
-            // Print the left half of the pattern
-            for (int column = 0; column <= space; column++) {
+                       
+            // Print the left half of the pattern with a space added for single digits
+            for (int column = 0; column <= line; column++) {
+                if (Math.pow(2,column) < 10) {
+                    System.out.print(" ");
+                }
                 System.out.print((int) Math.pow(2, column) + " ");
             }
 
-            // Print the right half of the pattern
-            for (int column = space - 1; column >= 0; column--) {
+            // Print the right half of the pattern with a space added for single digits
+            for (int column = line - 1; column >= 0; column--) {
+                if (Math.pow(2,column) < 10) {
+                    System.out.print(" ");
+                }
                 System.out.print((int) Math.pow(2, column) + " ");
             }
 
             // Print spaces after for formatting
-            for (int column = 0; column < 2 * (rows - space - 1); column++) {
+            for (int column = 0; column < 3 * (maxRows - line - 1); column++) {
                 System.out.print(" ");
             }
-            if (space < 4) {
-                System.out.print("   ");
-            }
-            if (space == 4) {
-                System.out.print("  ");
-            }
-            if (space == 5) {
-                System.out.print(" ");
-            }
-
+          
             // Print @ symbol
             System.out.println("@");
         }
